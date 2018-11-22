03c1bf8a31697ac4fbec4903a48c7227bed8275e
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-11-22 07:48:11 -0800

    Add example of using a threading condition

---
+++ section2/condition.py
@@ -0,0 +1,60 @@
+from threading import Condition, Thread
+import time
+
+ITEMS = []
+CONDITION = Condition()
+
+
+class consumer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def consume(self):
+        global CONDITION
+        global ITEMS
+
+        with CONDITION:
+            if len(ITEMS) == 0:
+                print("Consumer notify: no item to consume")
+                CONDITION.wait()
+            ITEMS.pop()
+            print("Consumer notify: consumed 1 item")
+            print("Consumer notify: items to consume are " + str(len(ITEMS)))
+            CONDITION.notify()
+
+    def run(self):
+        for _ in range(20):
+            time.sleep(2)
+            self.consume()
+
+
+class producer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def produce(self):
+        global CONDITION
+        global ITEMS
+
+        with CONDITION:
+            if len(ITEMS) == 10:
+                print("Producer notify: items produced are " + str(len(ITEMS)))
+                print("Producer notify: stop the production!")
+                CONDITION.wait()
+            ITEMS.append(1)
+            print("Producer notify: total items produced " + str(len(ITEMS)))
+            CONDITION.notify()
+
+    def run(self):
+        for _ in range(20):
+            time.sleep(1)
+            self.produce()
+
+
+if __name__ == '__main__':
+    producer = producer()
+    consumer = consumer()
+    producer.start()
+    consumer.start()
+    producer.join()
+    consumer.join()
